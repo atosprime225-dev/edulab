2650ee83475d6ae333a1855d44a22df3bea692ef
Zakaria Lahlou <atosprime225@gmail.com>
2025-10-11 21:14:05 +0100

    Ajout logo naviguation

--- glycine-repaire/components/Header.tsx
+++ glycine-repaire/components/Header.tsx
@@ -1,9 +1,12 @@
 // components/Header.tsx
+import Image from 'next/image';
 import styles from './Header.module.css';
 export default function Header() {
   return (
   <header className={styles.Header}>
-      <div> Edulab</div>
+      <div className={styles.logoContainer}>
+        <Image src="/Edulab triangle imbriqué.png" alt="Logo Edulab" width={92} height={92} style={{height: 'auto' , width: 'auto', maxHeight: '60px'}} />        
+      </div>
     <nav className={styles.navlinks}>
       <div className={styles.dropdown}>
       <a href="#">Ressources</a>
