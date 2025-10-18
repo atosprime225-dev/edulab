238f268d07fbdb8ffc9ab8ad6e1c7f3a14c5cc4d
Zakaria Lahlou <atosprime225@gmail.com>
2025-10-18 22:40:06 +0100

    update 2 cms

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,9 +7,10 @@ import { schemaTypes } from './schemas'
 export default defineConfig({
   name: 'default',
   title: 'Edulab CMS',
-  projectId: process.env.NEXT_PUBLIC_SANITY_9yr8p8eo!, // défini dans .env.local
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, // défini dans .env.local
   dataset: 'production',
-  basePath: '/studio', // URL du studio sur ton site
+  basePath: '/studio',
+ // URL du studio sur ton site
   plugins: [structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
