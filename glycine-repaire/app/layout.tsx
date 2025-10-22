1abda743c7b08f376072c69cc7892a2e5ba019cd
Zakaria Lahlou <atosprime225@gmail.com>
2025-10-22 21:08:28 +0100

    test2

--- glycine-repaire/app/layout.tsx
+++ glycine-repaire/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import Header from "../components/Header";
 
 export const metadata = {
-  title: "Edulab",
+  title: "edulab",
   description: "Espace pédagogique collaboratif - École Les Glycines",
 };
 
